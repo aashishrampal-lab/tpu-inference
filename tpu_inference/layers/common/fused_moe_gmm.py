9103b84223f48c4912796fb4e1c9491f43392499
Aashish Rampal <aashishrampal@google.com>
2026-08-27 11:51:48 +0000

    fix(fused_moe_gmm): import tpu_inference.envs as envs

--- tpu_inference/layers/common/fused_moe_gmm.py
+++ tpu_inference/layers/common/fused_moe_gmm.py
@@ -18,6 +18,7 @@ from typing import Literal
 import jax
 from jax import numpy as jnp
 from jax.sharding import Mesh, NamedSharding, PartitionSpec as P
+import tpu_inference.envs as envs
 try:
     from tokamax._src.ops.experimental.gmm_v2.gmm_v2 import gmm_v2
 except ImportError:
